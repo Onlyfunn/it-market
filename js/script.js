16a8e74d5208cd01faa65248a0a7485f530486eb
Onlyfunn <89238334+Onlyfunn@users.noreply.github.com>
2025-04-02 17:43:21 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-var swiper = new Swiper(".swiper-card1", {
-  spaceBetween: 20,
-  initialSlide: 1,
-  slidesPerView: 4,
-  navigation: {
-    nextEl: ".swiper-button-next",
-  },
-});
-console.log(swiper);
-
-var swiper2 = new Swiper(".swiper-card2", {
-  spaceBetween: 0,
-  thumbs: {
-    swiper: swiper,
-  },
-});
-
-lightGallery(document.querySelector(".gallery"), {
-  licenseKey: "0000-0000-0000-0000",
-});
+if (document.querySelector(".swiper-card1")) {
+  var swiper = new Swiper(".swiper-card1", {
+    spaceBetween: 20,
+    initialSlide: 1,
+    slidesPerView: 4,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+if (document.querySelector(".swiper-card2")) {
+  var swiper2 = new Swiper(".swiper-card2", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+}
+console.log(Swiper);
+if (document.querySelector(".user-stories-swiper")) {
+  new Swiper(".user-stories-swiper", {
+    spaceBetween: 10,
+    loop: false,
+    slidesPerView: "auto",
+  });
+}
+if (document.querySelector(".gallery")) {
+  lightGallery(document.querySelector(".gallery"), {
+    licenseKey: "0000-0000-0000-0000",
+  });
+}
