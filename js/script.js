0814beea5580cf9bd627ec2d3b73aec46ce7c649
Onlyfunn <89238334+Onlyfunn@users.noreply.github.com>
2025-04-02 18:05:19 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,6 @@ if (document.querySelector(".swiper-card2")) {
     },
   });
 }
-console.log(Swiper);
 if (document.querySelector(".user-stories-swiper")) {
   new Swiper(".user-stories-swiper", {
     spaceBetween: 10,
@@ -28,5 +27,22 @@ if (document.querySelector(".user-stories-swiper")) {
 if (document.querySelector(".gallery")) {
   lightGallery(document.querySelector(".gallery"), {
     licenseKey: "0000-0000-0000-0000",
+  });
+}
+
+if ($("#header").length) {
+  const $header = $("#header");
+  let prevScroll;
+  let lastShowPos;
+  $(window).on("scroll", function () {
+    console.log("123");
+    const scrolled = $(window).scrollTop();
+    if (scrolled > 100 && scrolled > prevScroll) {
+      $header.addClass("header-out");
+      lastShowPos = scrolled;
+    } else if (scrolled <= Math.max(lastShowPos - 250, 0)) {
+      $header.removeClass("header-out");
+    }
+    prevScroll = scrolled;
   });
 }
